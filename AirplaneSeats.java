6c6b4f5289f734b8e6519f0a759783f5c5daaa93
ChristianVinceBaquero <113317906+ChristianVinceBaquero@users.noreply.github.com>
2023-04-26 15:09:03 +0800

    Add files via upload

---
+++ AirplaneSeats.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+   public class AirplaneSeats{
+   
+      static Scanner console = new Scanner(System.in);
+   
+         public static void main(String[]args){
+         int [][] seat = new int[13][6];
+            
+            for (int r = 0; r < seat.length; r++){
+            for (int c = 0; c < 6; c++){
+            System.out.print("\t" + seat[r][c]);
+         
+         }
+         
+               System.out.print("\n");
+                   
+                   }
+         
+            }
+         
+      }
+
+      
+         
+
+         
+   
+      
